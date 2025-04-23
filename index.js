8d6da43cdfb950b1e61a5a0b80684c1d55a3cb78
Aiham Hamoudh <aiham5hamoudh@gmail.com>
2025-04-23 16:49:40 -0500

    server 2

--- index.js
+++ index.js
@@ -8,24 +8,31 @@ require("./passport");
 const app = express();
 
 const allowedOrigins = [
-  "https://client-instant.netlify.app/",
+  "https://client-instant.netlify.app",
   "http://localhost:5173",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  console.log("Incoming request from origin:", origin);
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  } else {
+    console.error("Blocked by CORS:", origin);
+    res.status(403).send("Not allowed by CORS");
+  }
+});
 
 app.use(
   session({
